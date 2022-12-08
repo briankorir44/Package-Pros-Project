f04783b5d5efde2c22dab144989ea6626aae0608
Juspher Mokua <juspermokua6@gmail.com>
2022-12-08 21:39:09 +0300

    done

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 // import '.com/styles/Navbar.css'
 import './components/styles/Navbar.css'
-import Sidebar from './pages/Sidebar';
-import Dashboard from './pages/Dashboard.js';
-import About from './pages/About.js';
-import Orders from './pages/Orders.js';
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -25,16 +22,9 @@ function App() {
 <Routes>
       <Route
           exact path="/" element={<Navbar/>}></Route>
-       
-      </Routes>
-      <Sidebar>
-        <Routes>
           <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Orders" element={<Orders />} />
-          
-        </Routes>
-      </Sidebar>
+      </Routes>
+      
     </div>
   );
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,21 @@
+import { Route, Routes,  } from "react-router-dom";
 import React from 'react'
 import '../components/styles/Main.css'
+import Sidebar from './Sidebar';
+import About from './About.js';
+import Orders from './Orders.js';
 
 function Dashboard() {
   return (
     <div>
+      <Sidebar>
+        <Routes>
+          
+          <Route path="/About" element={<About />} />
+          <Route path="/Orders" element={<Orders />} />
+          
+        </Routes>
+      </Sidebar>
       <h1>Dashboard Page</h1>
     </div>
   )
