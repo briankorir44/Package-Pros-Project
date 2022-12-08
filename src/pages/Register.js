75eb3699b57e1132868aed513775d9d7d079dbd3
briankorir44 <briankorir44@yahoo.com>
2022-12-08 11:31:21 +0300

    create usestate

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import '../components/styles/Login.css'
+// import React, { useState } from "react";
+// import '../components/styles/Login.css'
 
 
 export const Register = (props) => {
