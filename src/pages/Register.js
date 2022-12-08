2be51b4e93f544cd49f09971524b1bd8343b2eed
briankorir44 <briankorir44@yahoo.com>
2022-12-08 11:39:41 +0300

    create & style label

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -18,7 +18,7 @@ export const Register = (props) => {
                 <input value={name} name="name" id="name" placeholder="Full Name" />
                 < label htmlFor="email">E-mail</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                < label htmlFor="password">Password</label>
+                {/* < label htmlFor="password">Password</label> */}
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*******" id="password" name="password" />
                 <button type="submit">Register</button>
             </form>
