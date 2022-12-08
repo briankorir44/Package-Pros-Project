bdc450b0fd2333fd14a93d49104e55697f8287c4
briankorir44 <briankorir44@yahoo.com>
2022-12-08 11:33:23 +0300

    style container

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -12,18 +12,18 @@ export const Register = (props) => {
         console.log(email);
     }
     return (
-        // <div className="container">
-        //     <form className="register-form" onSubmit={handleSubmit}>
-        //         <label htmlFor="name">Full Name</label>
-        //         <input value={name} name="name" id="name" placeholder="Full Name" />
-        //         < label htmlFor="email">E-mail</label>
-        //         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-        //         < label htmlFor="password">Password</label>
-        //         <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*******" id="password" name="password" />
-        //         <button type="submit">Register</button>
-        //     </form>
-        //     <button className='link-btn' onClick={() => props.onFormSwitch('login')}>Already have an accout? Log In Here</button>
-        // </div>
+        <div className="container">
+            <form className="register-form" onSubmit={handleSubmit}>
+                <label htmlFor="name">Full Name</label>
+                <input value={name} name="name" id="name" placeholder="Full Name" />
+                < label htmlFor="email">E-mail</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                < label htmlFor="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*******" id="password" name="password" />
+                <button type="submit">Register</button>
+            </form>
+            <button className='link-btn' onClick={() => props.onFormSwitch('login')}>Already have an accout? Log In Here</button>
+        </div>
     )
 }
 
